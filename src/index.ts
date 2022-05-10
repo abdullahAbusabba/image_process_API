f81d8f70a4831ae3c84bf7a13c7f0f6ad788336d
abdullahAbusabba <alajmi7abdullah@gmail.com>
2022-05-10 08:26:13 +0300

    adding remaining files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,16 @@
-const sharp = require("sharp");
-const fs = require("fs");
-const request = require('request');
+import express from "express";
+import routers from "./api/routers";
+import {logger} from "./helpers/logger";
+const app = express();
+const port = process.env.PORT || 3001;
 
+app.get("/",(req: express.Request, res: express.Response) => {
+  res.send("main index");
+  console.log("main index.ts");
+});
+
+app.use("/api",logger,routers);
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
