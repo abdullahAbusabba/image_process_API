9b36deac56a80770e9e5d4ef2acc0fe0192943ea
abdullahAbusabba <alajmi7abdullah@gmail.com>
2022-05-10 08:24:34 +0300

    logger function

---
+++ src/helpers/logger.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+/**
+
+* middleware for all routes
+* @param req - Request object.
+* @params res - Response object.
+* @params next - Passes control to the next matching route.
+* @returns void.
+
+*/
+
+function logger (
+  req: express.Request,
+  res: express.Response,
+  next: Function
+): void {
+  const url = req.url;
+  console.log(`${url} was visited`);
+  next();
+}
+
+export { logger };
